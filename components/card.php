a685deaed53d45ff00703397204a5eb31e944fd2
Mega-117 <enricosaian@hotmail.it>
2021-12-01 16:05:11 +0100

    sistemato css

--- components/card.php
+++ components/card.php
@@ -2,9 +2,9 @@
     <div class="card text-center px-3 pt-3 pb-0 my-card">
         <img src="<?php echo $albumSingolo["poster"] ?>" class=" card-img-top" alt="ciao" />
         <div class="card-body px-3 pt-3 pb-0">
-            <h5 class="card-title"><?php echo $albumSingolo["title"] ?></h5>
-            <p class="card-text mb-0"><?php echo $albumSingolo["author"] ?></p>
-            <p class="card-text"><?php echo $albumSingolo["year"] ?></p>
+            <h5 class="my-title card-title"><?php echo $albumSingolo["title"] ?></h5>
+            <p class="my-text card-text mb-0"><?php echo $albumSingolo["author"] ?></p>
+            <p class="my-text card-text"><?php echo $albumSingolo["year"] ?></p>
         </div>
     </div>
 </div>
